57ada69be14585cc0f63ab848614b389e4c021be
Aradl1 <austinradloff@gmail.com>
2026-09-17 21:47:39 -0400

    ch4 changes

--- ch4/ch4youdoit/DogTriathlonParticipant.java
+++ ch4/ch4youdoit/DogTriathlonParticipant.java
@@ -1,41 +1,41 @@
 //Austin Radloff
 //p.141
 public class DogTriathlonParticipant {
-    private final int NUM_EVENTS;
-    private static int totalCumulativeScore = 0;
+        private final int NUM_EVENTS;
+        private static int totalCumulativeScore = 0;
 
-    private String name;
-    private int obedienceScore;
-    private int conformationScore;
-    private int agilityScore;
-    private int total;
-    private double avg;
+        private String name;
+        private int obedienceScore;
+        private int conformationScore;
+        private int agilityScore;
+        private int total;
+        private double avg;
 
-    public DogTriathlonParticipant(String name,
-            int numEvents, int score1, int score2, int score3) {
-        this.name = name;
-        NUM_EVENTS = numEvents;
-        obedienceScore = score1;
-        conformationScore = score2;
-        agilityScore = score3;
+        public DogTriathlonParticipant(String name,
+                        int numEvents, int score1, int score2, int score3) {
+                this.name = name;
+                NUM_EVENTS = numEvents;
+                obedienceScore = score1;
+                conformationScore = score2;
+                agilityScore = score3;
 
-        total = obedienceScore +
-                conformationScore + agilityScore;
+                total = obedienceScore +
+                                conformationScore + agilityScore;
 
-        avg = (double) total / NUM_EVENTS;
+                avg = (double) total / NUM_EVENTS;
 
-        totalCumulativeScore = totalCumulativeScore +
-                total;
-    }
+                totalCumulativeScore = totalCumulativeScore +
+                                total;
+        }
 
-    public void display() {
-        System.out.println(name + " participated in " +
-                NUM_EVENTS +
-                " events and has an average score of " + avg);
+        public void display() {
+                System.out.println(name + " participated in " +
+                                NUM_EVENTS +
+                                " events and has an average score of " + avg);
 
-        System.out.println(" " + name +
-                " has a total score of " + total +
-                " bringing the total cumulative score to " +
-                totalCumulativeScore);
-    }
+                System.out.println(" " + name +
+                                " has a total score of " + total +
+                                " bringing the total cumulative score to " +
+                                totalCumulativeScore);
+        }
 }
